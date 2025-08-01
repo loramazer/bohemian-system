16badd007be0b163529c0173cbde52d6a5d3f205
loramazer <lauratopoma@gmail.com>
2025-07-31 21:07:03 -0300

    criei gitignore, fiz o db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,20 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
-connection.connect((err) => {
-  if (err) throw err;
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Erro ao conectar com o banco de dados:', err);
+    throw err; // Lança o erro para parar a aplicação se não conseguir conectar
+  }
   console.log('MySQL conectado com sucesso!');
+  connection.release(); // Importante: devolve a conexão para o pool
 });
 
-module.exports = connection;
+module.exports = pool;
